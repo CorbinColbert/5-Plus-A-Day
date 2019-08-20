eed672450500924ca63c43c449dd4cb48d2b5912
CorbinColbert <CorbinColbert@gmail.com>
2019-08-20 17:01:57 +1200

    made a prefab of a board

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,27 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//[ExecuteInEditMode]
 public class Board : MonoBehaviour
 {
     public GameObject TilePrefab;
     public int BoardLenght;
     public int BoardHight;
     GameObject TempObject;
+    public GameObject[,] BoardArray;
 
     // Start is called before the first frame update
     void Start()
     {
+        BoardArray = new GameObject[BoardLenght, BoardHight];
         //create the board
         for (int length = 1; length <= BoardLenght; length++)
         {
             for (int hight = 1; hight <= BoardHight; hight++)
             {
-                TempObject = (GameObject)Instantiate(TilePrefab, new Vector3((length * 1.1f)-(BoardLenght/2), 0, hight * 1.1f), Quaternion.identity);
+                TempObject = (GameObject)Instantiate(TilePrefab, new Vector3((length * 1.1f)-(BoardLenght/2.0f), 0, hight * 1.1f), Quaternion.identity);
                 TempObject.GetComponent<Tile>().XTilePos = length;
                 TempObject.GetComponent<Tile>().YTilePos = hight;
                 TempObject.transform.parent = this.transform;
                 TempObject.name = "Tile_" + length + "_"+hight;
+                BoardArray[length - 1, hight - 1] = TempObject;
             }
         }
     }
