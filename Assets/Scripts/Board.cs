3d79456963737445d8bf2435c522e7ed6f7dcb49
CorbinColbert <CorbinColbert@gmail.com>
2019-08-20 14:10:54 +1200

    added the creation of the board

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class Board : MonoBehaviour
 {
+    public GameObject TilePrefab;
+    public int BoardLenght;
+    public int BoardHight;
+    GameObject AddName;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //create the board
+        for (int length = 0; length < BoardLenght; length++)
+        {
+            for (int hight = 0; hight < BoardHight; hight++)
+            {
+                AddName = (GameObject)Instantiate(TilePrefab, new Vector3((length * 1.1f)-(BoardLenght/2), 0, hight * 1.1f), Quaternion.identity);
+                AddName.transform.parent = this.transform;
+                AddName.name = "Tile_" + length + "_"+hight;
+            }
+        }
     }
 
     // Update is called once per frame
