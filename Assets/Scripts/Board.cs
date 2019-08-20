15f87622f67de36eda23819109f5a67fabd6f44b
CorbinColbert <CorbinColbert@gmail.com>
2019-08-20 14:24:19 +1200

    updated board script
    changed how the tiles are named

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -7,19 +7,21 @@ public class Board : MonoBehaviour
     public GameObject TilePrefab;
     public int BoardLenght;
     public int BoardHight;
-    GameObject AddName;
+    GameObject TempObject;
 
     // Start is called before the first frame update
     void Start()
     {
         //create the board
-        for (int length = 0; length < BoardLenght; length++)
+        for (int length = 1; length <= BoardLenght; length++)
         {
-            for (int hight = 0; hight < BoardHight; hight++)
+            for (int hight = 1; hight <= BoardHight; hight++)
             {
-                AddName = (GameObject)Instantiate(TilePrefab, new Vector3((length * 1.1f)-(BoardLenght/2), 0, hight * 1.1f), Quaternion.identity);
-                AddName.transform.parent = this.transform;
-                AddName.name = "Tile_" + length + "_"+hight;
+                TempObject = (GameObject)Instantiate(TilePrefab, new Vector3((length * 1.1f)-(BoardLenght/2), 0, hight * 1.1f), Quaternion.identity);
+                TempObject.GetComponent<Tile>().XTilePos = length;
+                TempObject.GetComponent<Tile>().YTilePos = hight;
+                TempObject.transform.parent = this.transform;
+                TempObject.name = "Tile_" + length + "_"+hight;
             }
         }
     }
