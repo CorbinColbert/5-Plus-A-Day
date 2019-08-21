b5379ac5ed7000393941ee9c749cd3761ab6c568
CorbinColbert <CorbinColbert@gmail.com>
2019-08-21 16:00:54 +1200

    Created new board prefab

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -5,26 +5,26 @@ using UnityEngine;
 public class Board : MonoBehaviour
 {
     public GameObject TilePrefab;
-    public int BoardLenght;
-    public int BoardHight;
+    public int BoardLength;
+    public int BoardHeight;
     GameObject TempObject;
     public GameObject[,] BoardArray;
 
     // Start is called before the first frame update
     void Start()
     {
-        BoardArray = new GameObject[BoardLenght, BoardHight];
+        BoardArray = new GameObject[BoardLength, BoardHeight];
         //create the board
-        for (int length = 1; length <= BoardLenght; length++)
+        for (int height = 0; height < BoardHeight; height++)
         {
-            for (int height = 1; height <= BoardHight; height++)
+            for (int length = 0; length < BoardLength; length++)
             {
-                TempObject = (GameObject)Instantiate(TilePrefab, new Vector3((length * 1.1f)-(BoardLenght/2.0f), 0, height * 1.1f), Quaternion.identity);
-                TempObject.GetComponent<Tile>().XTilePos = length;
-                TempObject.GetComponent<Tile>().YTilePos = height;
+                TempObject = (GameObject)Instantiate(TilePrefab, new Vector3((length * 1) - ((BoardLength / 2) - 0.5f), 0, (height * 1) - ((BoardHeight / 2) - 0.5f)), Quaternion.identity);
+                TempObject.GetComponent<Tile>().XTilePos = length + 1;
+                TempObject.GetComponent<Tile>().YTilePos = height + 1;
                 TempObject.transform.parent = this.transform;
-                TempObject.name = "Tile_" + length + "_"+height;
-                BoardArray[length - 1, height - 1] = TempObject;
+                TempObject.name = "Tile_" + (length + 1) + "_" + (height + 1);
+                BoardArray[length , height] = TempObject;
             }
         }
     }
---
+++ Assets/Scripts/PremadeBoard.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PremadeBoard : MonoBehaviour
+{
+    public GameObject[] BoardArray;
+    private GameObject[,] Board2DArray;
+
+    private void Start()
+    {
+        Board2DArray = new GameObject[8, 12];
+        int index = 0;
+        //add elements from BoardArray to Board2DArray
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 12; j++)
+            {
+                Board2DArray[i, j] = BoardArray[index];
+                index++;
+            }
+        }
+    }
+}
