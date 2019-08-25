d270e57cec5c1d020a38b7632f4217463910cbb6
CorbinColbert <CorbinColbert@gmail.com>
2019-08-25 16:11:18 +1200

    tile script & board prefab changes

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -7,21 +7,29 @@ public class Tile : MonoBehaviour
 
     public int xTilePos;
     public int yTilePos;
-    public bool isLight;
-    public Material lightTile;
-    public Material darkTile;
+    private Material tileMaterial;
     public Material selectedTile;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        tileMaterial = gameObject.GetComponent<Renderer>().material;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnMouseEnter()
+    {
+        gameObject.GetComponent<Renderer>().material = selectedTile;
+    }
+
+    private void OnMouseExit()
+    {
+        gameObject.GetComponent<Renderer>().material = tileMaterial;
     }
 }
