ee3ab75ebf42ab605d13defd0c71cea3bf2cf9ba
CorbinColbert <CorbinColbert@gmail.com>
2019-08-25 15:50:44 +1200

    update to board script
    change variable names to work with the tile script

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -20,8 +20,8 @@ public class Board : MonoBehaviour
             for (int length = 0; length < BoardLength; length++)
             {
                 TempObject = (GameObject)Instantiate(TilePrefab, new Vector3((length * 1) - ((BoardLength / 2) - 0.5f), 0, (height * 1) - ((BoardHeight / 2) - 0.5f)), Quaternion.identity);
-                TempObject.GetComponent<Tile>().XTilePos = length + 1;
-                TempObject.GetComponent<Tile>().YTilePos = height + 1;
+                TempObject.GetComponent<Tile>().xTilePos = length + 1;
+                TempObject.GetComponent<Tile>().yTilePos = height + 1;
                 TempObject.transform.parent = this.transform;
                 TempObject.name = "Tile_" + (length + 1) + "_" + (height + 1);
                 BoardArray[length , height] = TempObject;
