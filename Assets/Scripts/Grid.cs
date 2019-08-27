df52997388d01baf4e016f397ac76d2c2844d230
CorbinCclbert <CorbinColbert@gmail.com>
2019-08-28 11:43:47 +1200

    start of node grid

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    public Vector2 gridSize;
+    public float nodeRadius;
+    Node[,] grid;
+
+    private float nodeDiameter;
+    private int gridSizeX, gridSizeY;
+
+
+    private void Start()
+    {
+        nodeDiameter = nodeRadius * 2;
+        gridSizeX = Mathf.RoundToInt(gridSize.x / nodeDiameter);
+        gridSizeY = Mathf.RoundToInt(gridSize.y / nodeDiameter);
+        CreateGrid();
+    }
+    void CreateGrid()
+    {
+        grid = new Node[gridSizeX, gridSizeY];
+        Vector3 gridBottomLeft = transform.position - Vector3.right * gridSize.x / 2 - Vector3.forward * gridSize.y / 2;
+
+        for (int x = 0; x < gridSizeX; x++)
+        {
+            for (int y = 0; y < gridSizeY; y++)
+            {
+                Vector3 nodePlacementPoint = gridBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
+                bool isNodeWalkable = !(Physics.CheckSphere(nodePlacementPoint,nodeRadius));
+                grid[x, y] = new Node(isNodeWalkable, nodePlacementPoint);
+            }
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(gridSize.x,1,gridSize.y));
+        if (grid != null)
+        {
+            foreach (Node node in grid)
+            {
+                Gizmos.color = (node.walkable) ? Color.white : Color.red;
+                Gizmos.DrawCube(node.worldPosition, Vector3.one * (nodeDiameter - .1f));
+            }
+        }
+    }
+}
