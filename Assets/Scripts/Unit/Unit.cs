d1fcf99fc4a0ccdbb302f869b1dc0110af2b3671
Cameron Guthrie <cameronguthrie.nz@gmail.com>
2019-09-17 14:19:02 +1200

    Deleted old board stuff

--- Assets/Scripts/Unit/Unit.cs
+++ Assets/Scripts/Unit/Unit.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class Unit : MonoBehaviour
 {
     public Loyalty loyalty;
-    private Tile tile;
     public Unit target;
 
     public int maximumHP;
@@ -60,15 +59,6 @@ public class Unit : MonoBehaviour
     //Call this when the target dies
     public void RemoveTarget() {
         target = null;
-    }
-
-    public Tile GetTile() {
-        return tile;
-    }
-
-    public void placeOnBoard(Tile tile) {
-        this.tile = tile;
-        tile.SetUnit(this);
     }
 
     private void OnDeath(Attack finalBlow) {        
