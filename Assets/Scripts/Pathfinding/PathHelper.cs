ee339011ca52f25b7092baf8321127032c485d11
CorbinColbert <CorbinColbert@gmail.com>
2019-09-17 18:28:11 +1200

    pathing script changes

--- Assets/Scripts/Pathfinding/PathHelper.cs
+++ Assets/Scripts/Pathfinding/PathHelper.cs
@@ -1,18 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PathHelper : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Queue<PathRequest> pathRequests = new Queue<PathRequest>();
+    PathRequest tempPathRequest;
+    bool requesting = false;
+    static PathHelper pathHelper;
+    Pathfinding pathfinder;
+
+    private void Awake()
     {
-        
+        pathHelper = this;
+        pathfinder = GetComponent<Pathfinding>();
+    }
+    public static void RequestAPath(GameObject targetTroop, GameObject pathRequester, Action<List<Node>, bool> requestResponce)
+    {
+        PathRequest request = new PathRequest(targetTroop, pathRequester, requestResponce);
     }
 
-    // Update is called once per frame
-    void Update()
+    void PathRequestFinished(List<Node> path, bool sucessful)
     {
-        
+
+    }
+
+    void nextInQueue()
+    {
+        if (!requesting && pathRequests.Count > 0)
+        {
+            //process next path request
+        }
     }
 }
+
---
+++ Assets/Scripts/Pathfinding/PathRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public struct PathRequest
+{
+    public GameObject pathingUnit;
+    public GameObject targetUnit;
+    public Action<List<Node>, bool> response;
+
+    public PathRequest(GameObject targetUnit, GameObject pathingUnit, Action<List<Node>, bool> response)
+    {
+        this.targetUnit = targetUnit;
+        this.pathingUnit = pathingUnit;
+        this.response = response;
+    }
+}
