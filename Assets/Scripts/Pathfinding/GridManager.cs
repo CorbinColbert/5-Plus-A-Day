ec4c37667d96bf7ef7c7c4d7b1a589fbe197b7b6
CorbinCclbert <CorbinColbert@gmail.com>
2019-09-18 12:44:17 +1200

    grid manager update

--- Assets/Scripts/Pathfinding/GridManager.cs
+++ Assets/Scripts/Pathfinding/GridManager.cs
@@ -2,24 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//This class is here to keep track of the different troops on the grid
 public class GridManager : MonoBehaviour
 {
-    //TODO: make sure troops dont go over their maximum
-    //TODO: keep track of troops
-    //
-    GameObject[] playerTroops;
-    GameObject[] enemyTroops;
-
-    private void Start()
-    {
-        //TODO: max amount of troops allowed on the grid of each type
-        playerTroops = new GameObject[24]; 
-        enemyTroops = new GameObject[24];
-    }
-
-    public void Update()
-    {
-        //TODO: update the lists of units
-    }
+    
 }
+
