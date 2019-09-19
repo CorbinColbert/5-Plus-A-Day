b6c0f44cf119e9e5ec1420559acb4e916970cb7b
Ashley <49117257+AshesOfDusk14@users.noreply.github.com>
2019-09-20 01:03:32 +1200

    UIDone?

---
+++ Assets/MenuScenes/Scripts/SceneTransitions.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneTransitions : MonoBehaviour
+{
+    public Animator transitionAnim;
+    public Button gameButton;
+    public string sceneName;
+    //public bool clicked = false;
+
+    private void Update()
+    {
+        if(EventSystem.current.currentSelectedGameObject.name == "Start")
+        {
+            StartCoroutine(LoadScene());
+        }
+    }
+
+    IEnumerator LoadScene()
+    {
+        transitionAnim.SetTrigger("end");
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene(sceneName);
+    }
+}
