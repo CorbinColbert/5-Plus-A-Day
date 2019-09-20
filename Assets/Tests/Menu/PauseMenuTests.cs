6d03f78b875beb7005561ec81f247ae0c22d7d5f
CorbinColbert <CorbinColbert@gmail.com>
2019-09-20 17:00:53 +1200

    test update

--- Assets/Tests/Menu/PauseMenuTests.cs
+++ Assets/Tests/Menu/PauseMenuTests.cs
@@ -6,11 +6,11 @@ using UnityEngine.TestTools;
 
 namespace Tests
 {
-    public class PathingTests
+    public class PauseMenuTests
     {
         // A Test behaves as an ordinary method
         [Test]
-        public void PathingTestsSimplePasses()
+        public void PauseMenuTestsSimplePasses()
         {
             // Use the Assert class to test conditions
         }
@@ -18,7 +18,7 @@ namespace Tests
         // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
         // `yield return null;` to skip a frame.
         [UnityTest]
-        public IEnumerator PathingTestsWithEnumeratorPasses()
+        public IEnumerator PauseMenuTestsWithEnumeratorPasses()
         {
             // Use the Assert class to test conditions.
             // Use yield to skip a frame.
