d1ed037a5c84eb1c92c7e7acf310c0e575ee08bd
CorbinColbert <CorbinColbert@gmail.com>
2019-09-20 20:28:13 +1200

    script updates

--- Assets/Tests/Menu/SceneTransitionsTests.cs
+++ Assets/Tests/Menu/SceneTransitionsTests.cs
@@ -8,21 +8,6 @@ namespace Tests
 {
     public class SceneTransitionsTests
     {
-        // A Test behaves as an ordinary method
-        [Test]
-        public void SceneTransitionsTestsSimplePasses()
-        {
-            // Use the Assert class to test conditions
-        }
 
-        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-        // `yield return null;` to skip a frame.
-        [UnityTest]
-        public IEnumerator SceneTransitionsTestsWithEnumeratorPasses()
-        {
-            // Use the Assert class to test conditions.
-            // Use yield to skip a frame.
-            yield return null;
-        }
     }
 }
--- Assets/Tests/Menu/SettingsMenuTests.cs
+++ Assets/Tests/Menu/SettingsMenuTests.cs
@@ -8,21 +8,6 @@ namespace Tests
 {
     public class SettingsMenuTests
     {
-        // A Test behaves as an ordinary method
-        [Test]
-        public void SettingsMenuTestsSimplePasses()
-        {
-            // Use the Assert class to test conditions
-        }
 
-        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-        // `yield return null;` to skip a frame.
-        [UnityTest]
-        public IEnumerator SettingsMenuTestsWithEnumeratorPasses()
-        {
-            // Use the Assert class to test conditions.
-            // Use yield to skip a frame.
-            yield return null;
-        }
     }
 }
