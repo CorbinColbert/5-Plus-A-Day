b84ea0d5e094ffc509d117c5132631f28486af9b
CorbinColbert <CorbinColbert@gmail.com>
2019-09-20 16:48:27 +1200

    test updates

--- Assets/Tests/Menu/MenuTests.cs
+++ Assets/Tests/Menu/MenuTests.cs
@@ -1,9 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
 
-public class MenuTests : MonoBehaviour
+namespace Tests
 {
+    public class MenuTests
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void MenuTestsSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
 
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator MenuTestsWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
 }
